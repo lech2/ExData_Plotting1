634558063814c9f6531b18f7b2ec71904ae001bd
lech2 <lech@uwb.edu.pl>
2014-05-10 13:10:29 +0200

    Project1(2)

--- plot3.R
+++ plot3.R
@@ -1,4 +1,24 @@
-d = read.table(file='Project1_DataFrame.csv', sep=';', dec='.', header=T)
+aFile = 'household_power_consumption.txt'
+
+myformat = list(Date='', Time='', Global_active_power=0, Global_reactive_power=0, Voltage=0,
+                Global_intensity=0, Sub_metering_1=0, Sub_metering_2=0, Sub_metering_3=0)
+
+f = file(aFile)
+
+open(f, open="r")
+titles = readLines(f, n=1)
+
+d = data.frame()
+
+repeat {
+        b = scan(f, what=myformat, sep=";", nlines=1, quiet=T, na.strings='?')   
+        if (b$Date == '3/2/2007') break
+        if ( (b$Date == "1/2/2007") | (b$Date == "2/2/2007") ) {    
+                b = as.data.frame(b)
+                d = rbind(d, b)
+        }
+}        
+close(f)
 
 aDate = d[,1]
 aTime = d[,2]
